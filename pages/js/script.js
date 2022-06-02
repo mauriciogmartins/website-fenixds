6b915bdf89e31f320e375364e63f7411c326e417
Mauricio Martins <mauriciogmartins.tj@gmail.com>
2022-06-02 15:31:08 -0300

    Website pronto

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,3 +1,24 @@
-function carregarHTML(pagina) {
-    $("#conteudo").load(pagina);
+let count = 1;
+
+
+setInterval(() => {
+    proximaImagem();
+}, 6000);
+
+function proximaImagem() {
+    count++;
+    if (count > 4) {
+        count = 1
+    }
+
+    document.getElementById(`radio-slides-${count}`).checked = true;
 }
+
+function carregarHTML(pagina) {
+    $("#home").load(pagina);
+}
+
+function home() {
+    window.location.href = "index.html";
+}
+
