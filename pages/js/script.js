4f6614269140c3175bd89c5fd46544493bc608b2
Mauricio Martins <mauriciogmartins.tj@gmail.com>
2022-06-06 19:02:41 -0300

    Melhorado a responsividade

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,8 +1,10 @@
 let count = 1;
-
+let ativo = true;
 
 setInterval(() => {
-    proximaImagem();
+    if (ativo === true) {
+        proximaImagem();
+    }
 }, 6000);
 
 function proximaImagem() {
@@ -12,6 +14,14 @@ function proximaImagem() {
     }
 
     document.getElementById(`radio-slides-${count}`).checked = true;
+}
+
+function focarImagem(index) {
+    ativo = false;
+    count = index;
+    document.getElementById(`radio-slides-${count}`).checked = true;
+    clearInterval();
+    ativo = true;
 }
 
 function carregarHTML(pagina) {
